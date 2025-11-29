59d4d54a25096fd18255233c876153b09f0941a5
Jess-- <jess@inetdev.co.uk>
2025-11-29 11:01:00 +0000

    Update gettoken.py

--- gettoken.py
+++ gettoken.py
@@ -1,42 +1,63 @@
+import time
+from hashlib import md5
+
+
+def get_nonce():
+    return int(time.time() * 1000)
+
+
 def gettoken():
-    import time
     import base64
     import json
     import requests
+    import hashlib
     from io import StringIO
     from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
     from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
     BearerToken = ""
 
-    class ConsoleColor:    
+    class ConsoleColor:
         OKBLUE = "\033[34m"
         OKCYAN = "\033[36m"
-        OKGREEN = "\033[32m"        
+        OKGREEN = "\033[32m"
         MAGENTA = "\033[35m"
         WARNING = "\033[33m"
         FAIL = "\033[31m"
         ENDC = "\033[0m"
-        BOLD = "\033[1m" 
+        BOLD = "\033[1m"
 
     with open('/data/options.json') as options_file:
        json_settings = json.load(options_file)
 
-    # Get key to encode token with
+    # Determine source based on API server
+    source = "elinter" if json_settings["API_Server"] == "pv.inteless.com" else "sunsynk"
+
+    # Create a nonce for the public key request
+    public_key_nonce = get_nonce()
+
+    # Create a signature for the public key request
+    public_key_signature_input = f"nonce={public_key_nonce}&source={source}POWER_VIEW"
+    public_key_signature = hashlib.md5(public_key_signature_input.encode()).hexdigest()
+
+    # Get public key to encode token with
     response = requests.get(
-        'https://api.sunsynk.net/anonymous/publicKey',
+        f'https://{json_settings["API_Server"]}/anonymous/publicKey',
         params={
-            'source': 'sunsynk',
-            'nonce': int(time.time() * 1000)
+            'source': source,
+            'nonce': public_key_nonce,
+            'sign': public_key_signature
         }
     )
+
+    public_key_string = response.json()['data']
 
     # Write public key file
     public_key_file = StringIO()
     public_key_file.writelines(
         [
             '-----BEGIN PUBLIC KEY-----',
-            response.json()['data'],
+            public_key_string,
             '-----END PUBLIC KEY-----'
         ]
     )
@@ -52,19 +73,25 @@ def gettoken():
         padding=PKCS1v15()
     )).decode('utf-8')
 
+    token_nonce = get_nonce()
+    token_sign_string = f'nonce={token_nonce}&source=sunsynk{public_key_string[:10]}'
+    token_sign = md5(token_sign_string.encode()).hexdigest()
+
     # API URL
     url = f'https://{json_settings["API_Server"]}/oauth/token/new'
     # Prepare request payload
     payload = {
-        "areaCode": "sunsynk",
         "client_id": "csp-web",
         "grant_type": "password",
         "password": encrypted_password,
-        "source": "sunsynk",
-        "username": json_settings['sunsynk_user']
+        "source": source,
+        "username": json_settings['sunsynk_user'],
+        'nonce': token_nonce,
+        'sign': token_sign
     }
+
     # Headers
-    headers = {"Content-Type": "application/json"}    
+    headers = {"Content-Type": "application/json"}
     try:
         # Send POST request with timeout (10s)
         response = requests.post(url, json=payload, headers=headers, timeout=10)
